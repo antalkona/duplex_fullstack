7ff4b763f58f1452ff63725a4308b4e84e190174
alexei <antalkon789@mail.ru>
2023-12-22 23:40:41 +0500

    v4.1 stable

--- server.js
+++ server.js
@@ -27,6 +27,40 @@ app.get('/', (req, res) => {
 
 
 
+// TEST //
+
+app.get('/pages/:pageNumber', (req, res) => {
+    const pageNumber = req.params.pageNumber;
+    const pagePath = path.join(__dirname, 'public', 'pages', `${pageNumber}.html`);
+
+    fs.readFile(pagePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(404).send('Страница не найдена.');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+// ЗАПРОС ДЛЯ СОЗДАНИЯ СТРАНИЦЫ
+app.post('/pages', (req, res) => {
+    const content = req.body.content; // Получаем содержимое новой страницы из запроса
+    const pagesFolderPath = path.join(__dirname, 'public', 'pages'); // Путь к папке с страницами
+    const newPageNumber = fs.readdirSync(pagesFolderPath).length + 1; // Номер новой страницы
+    const newPagePath = path.join(pagesFolderPath, `${newPageNumber}.html`); // Путь к новой странице
+
+    fs.writeFile(newPagePath, content, (err) => { // Записываем содержимое в файл новой страницы
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.status(500).send('Ошибка: папка или путь к файлу не существует.'); // Если папка или путь к файлу не существует
+            } else {
+                res.status(500).send(`Ошибка при создании страницы: ${err.message}`); // Общая ошибка при создании страницы
+            }
+        } else {
+            res.status(201).send('Страница создана успешно!'); // Ответ об успешном создании страницы
+        }
+    });
+});
 
 
 // ПОСТ ЗАПРОСЫ
